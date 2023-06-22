c1a093ec6956cad3d1429bd0b059d3f948093855
KovaKreative <eric.kovalevskyy@gmail.com>
2023-06-22 15:23:48 -0400

    Add background

--- src/components/Hero/Tagline.js
+++ src/components/Hero/Tagline.js
@@ -1,7 +1,7 @@
 export default function Tagline() {
   return (
     <h1 className='Tagline'>
-      Hi, my name is <span className='highlight'>Eric Kovalevskyy</span>! <br/>
+      Hi, my name is <span className='highlight'>Eric Kovalevskyy</span><br/>
       <span className='highlight'>Full Stack Web Developer</span>, Artist, and Pop-culture Nerd
     </h1>
   );
